70699bd10fa5c317f97c2d40ef0805626a3d76cd
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-07-23 10:35:19 +0530

    Code

---
+++ connection.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://ajay:ajay123@cluster0-bbg3p.mongodb.net/test?retryWrites=true&w=majority');
+//mongoose.connect('mongodb://localhost:27017/onlineshop');
+module.exports = mongoose;
