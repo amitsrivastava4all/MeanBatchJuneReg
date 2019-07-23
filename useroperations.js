d58834984545d9d633f92f20c33f9506de7b65bc
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-07-23 10:16:55 +0530

    Code

---
+++ useroperations.js
@@ -0,0 +1,51 @@
+const UserCollection = require('../models/user');
+//const bcrypt = require('bcrypt');
+var passwordHash = require('password-hash');
+const saltRounds = 10;
+const userOperations = {
+    add(userObject, res){
+        var hash = passwordHash.generate(userObject.password);
+        //var hash = bcrypt.hashSync(userObject.password, saltRounds);
+        console.log("Hash is ",hash);
+        userObject.password = hash;
+        UserCollection.create(userObject,(err)=>{
+            if(err){
+                res.send('Error During Add');
+                console.log('Error During Add ',err);
+            }
+            else{
+                res.send('Record added...');
+            }
+        })
+    },
+    search(userObject,res){
+        UserCollection.findOne({'userid':userObject.userid}, (err,doc)=>{
+            if(err){
+                res.send('Something went Wrong');
+                console.log('Error During User Search ',err);
+            }
+            else if(doc){
+                let result =passwordHash.verify(userObject.pwd, doc.password);
+                //let result = bcrypt.compareSync(userObject.pwd, doc.password);
+                if(result){
+                let screenArr = [{linkname:'Add User',url:'/add'},{linkname:'Add Product',url:'/product'}]
+                res.render('dashboard',{'user':userObject.userid,'links':screenArr});
+                }
+                else{
+                    res.send('Invalid Userid or Password');
+                }
+                //res.send('Welcome '+userObject.userid);
+            }
+            else{
+                res.send('Invalid Userid or Password');
+            }
+        })
+    },
+    update(){
+
+    },
+    delete(){
+
+    }
+}
+module.exports = userOperations;
